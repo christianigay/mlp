f32c03c2e6ecadcf09256eebdd109566a4fde59f
christian <christian.igay@gmail.com>
2022-04-21 09:53:30 +0800

    add login controller

--- app/Http/Controllers/Frontoffice/LoginController.php
+++ app/Http/Controllers/Frontoffice/LoginController.php
@@ -2,12 +2,32 @@
 namespace App\Http\Controllers\Frontoffice;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
 
-    public function login()
+    public function login(Request $request)
     {
-        return response('test', 200);
+        
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+     
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken($request->device_name)->plainTextToken;
+        return response($token, 200);
     }
 }
